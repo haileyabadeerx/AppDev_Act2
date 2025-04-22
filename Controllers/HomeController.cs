e712c90372b21add6baa839135664ff3e65b8bc9
haileyabadeerx <haileykendramcduffie@gmail.com>
2025-04-22 21:53:32 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Activity2.Models;
+
+namespace Activity2.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Try()
+    {
+        return View();
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Surprise()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
